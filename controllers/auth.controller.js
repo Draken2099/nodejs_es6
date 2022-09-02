2777daebb95e5d73a4b55190a4b5ddd6c3ba9499
khensa <oven4151@gmail.com>
2022-09-02 13:03:39 +0700

    commit

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,24 @@
-import User from '../models/User.model'
+// import Users from '../models/User.model'
 
 exports.register = (req, res) => {
-    
+    const {fname, lname, phone} = req.body
+    console.log({
+        name: fname,
+        phone: phone
+    })
+    res.json({
+        name: fname + ' ' + lname,
+        phone: phone
+    })
 }
 
 exports.login = (req, res) => {
-    
+    console.log("User is login")
+    res.send("User is logging in")
 }
 
 exports.getUser = (req, res) => {
-    
+    res.json({
+        message: "Get user routes"
+    })
 }
